38b1df677fd264c95c65f57820c3fe99fa4810c1
Alena <hlokhman@wellesley.edu>
2021-07-06 16:48:39 -0400

    Update Assignment07.jl

--- src/Assignment07.jl
+++ src/Assignment07.jl
@@ -21,8 +21,36 @@ function normalizeDNA(seq)
     return seq # change to `return LongDNASeq(seq)` if you want to try to use BioSequences types
 end
 
+function composition(sequence)
+sequence = normalizeDNA(sequence)
+DNA = Dict('A'=> 0, 'C'=> 0, 'G'=> 0, 'T'=> 0, 'N'=> 0)
+for key in sequence
+    if key =='A' 
+        DNA['A'] = 0
+    end
+    if key == 'C' 
+        DNA['C'] = 0 
+    end  
+    if key == 'G' 
+        DNA['G'] = 0 
+    end 
+    if key == 'T' 
+        DNA['T'] = 0
+    end
+    if key == 'N' 
+        DNA['N'] = 0
+return DNA
+end
+
+function gc_content(sequence)
+    A,C,G,T,N = composition(sequence)
+    return (G + C)/length(sequence)
+end
+
+
 
 # Your code here.
+
 # Don't forget to export your functions!
 
 
